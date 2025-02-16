0f73bd48c21320087ba0445d38b7065b24ef5526
pixeloid <olah.gergely@pixeloid.hu>
2025-02-16 18:38:09 +0100

    Initialize Firebase functions and update configuration for Firebase Admin SDK

--- app/api/firebase.ts
+++ app/api/firebase.ts
@@ -1,14 +1,20 @@
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
+import * as functions from 'firebase-functions';
+const adminConfig = functions.config().admin || {};
 
-// Initialize Firebase Admin SDK if not already initialized
+// Initialize Firebase Admin SDK only once
 if (!getApps().length) {
-    const serviceAccount = require('@/bedtimestories1-5f241-firebase-adminsdk-fbsvc-73ac773ee4.json'); // Replace with your path
     initializeApp({
-        credential: cert(serviceAccount)
+        credential: cert({
+            projectId: adminConfig.project_id || process.env.FIREBASE_PROJECT_ID,
+            privateKey: (adminConfig.private_key || process.env.FIREBASE_PRIVATE_KEY)?.replace(/\\n/g, '\n'),
+            clientEmail: adminConfig.client_email || process.env.FIREBASE_CLIENT_EMAIL,
+        }),
     });
 }
+
 
 const db = getFirestore();
 const auth = getAuth();
