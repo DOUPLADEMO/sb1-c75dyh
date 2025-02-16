5b4d337b24323edfc08ad02f727fa846bf1bd3a9
pixeloid <olah.gergely@pixeloid.hu>
2025-02-16 15:01:43 +0100

    Refactor Firebase configuration to use environment variables for sensitive data

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDNJPGP2qbyBEDBg6NjmYvAumSrJX3eTr8",
-    authDomain: "bedtimestories1-5f241.firebaseapp.com",
-    projectId: "bedtimestories1-5f241",
-    storageBucket: "bedtimestories1-5f241.firebasestorage.app",
-    messagingSenderId: "565165375001",
-    appId: "1:565165375001:web:19a172587f6cb98d213488",
-    measurementId: "G-39NZLVW8RF"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
