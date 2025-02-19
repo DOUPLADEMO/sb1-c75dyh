a38eed1e9e1e1ccf521617fc6e91d8a5f5920867
pixeloid <olah.gergely@pixeloid.hu>
2025-02-19 11:31:28 +0100

    Fix Firebase private key parsing to correctly handle JSON format

--- app/api/firebase.ts
+++ app/api/firebase.ts
@@ -7,7 +7,7 @@ if (!getApps().length) {
     const firebaseCredentials = {
         projectId: process.env.FIREBASE_PROJECT_ID,
         privateKey: process.env.FIREBASE_PRIVATE_KEY
-            ? (process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'))
+            ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'))
             : undefined,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     };
